bf9cbc9cd88762844a9c737800527890a96a3172
j-rods <jrrodriguez84@gmail.com>
2019-08-07 19:37:30 +0100

    configure vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,17 +9,19 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,500,700|Material+Icons'}
     ]
   },
   loading: { color: '#fff' },
   css: [],
-  plugins: [],
+  plugins: ['~plugins/vuetify.js'],
   devModules: [
     '@nuxtjs/eslint-module'
   ],
   modules: [],
   build: {
+    vendor: ['vuetify'],
     extend (config, ctx) {
     }
   }
