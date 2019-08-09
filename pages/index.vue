4080d3d7875660a4dec4f899c19a6de214b86a36
j-rods <jrrodriguez84@gmail.com>
2019-08-09 13:09:17 +0100

    add search form

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,31 @@
 <template>
-  <div />
+  <div>
+    <h1>Search Albums</h1>
+    <br></br>
+    <v-form>
+      <v-text-field v-model="search" placeholder="Search Artist" autofocus>
+      </v-text-field>
+    </v-form>
+  </div>
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
+  data () {
+    return {
+      search: ''
+    };
+  }
 };
 </script>
 
 <style>
+* {
+  text-align: center;
+}
+
+h1 {
+  padding: 20px;
+}
 </style>
