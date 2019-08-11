cf1cec4feb09b9ae8d0e7098a48e6b6d820b55f6
j-rods <jrrodriguez84@gmail.com>
2019-08-11 10:27:35 +0100

    refactor to use asyncdata

--- pages/results/_id.vue
+++ pages/results/_id.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <h1>Hello from results route {{ $route.params.id }}</h1>
+    {{ albumData }}
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  asyncData ({ params }) {
+    return axios.get(`https://itunes.apple.com/search?term=${params.id}&entity=album`)
+        .then((response) => {
+          return { albumData: response.data.results };
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  },
   middleware: 'search'
 };
 </script>
